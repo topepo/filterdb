9a34bb1aac0d26cd5a7f12ccd90b22300cfc68c8
Max Kuhn <mxkuhn@gmail.com>
2023-07-17 12:30:11 -0400

    trim todo list

--- R/filterdb-package.R
+++ R/filterdb-package.R
@@ -18,8 +18,6 @@ utils::globalVariables(c(
 # TODOs
 # - how to filter with non-informative results? specifically filter on informative
 #   results and then keep the non-informative?
-# - filters specifically exclude or include?
-# - use a new vcrts class to embed the non-informative value into the vector?
 # - should we add main arguments for tuning parameters?
 # - check for empty ellipses when required in fit_xy
 
